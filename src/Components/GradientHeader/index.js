982e2c2022c3b88d3dc1b3fbfec4c6f2ecb8bd51
KZTN <kakaiocesar2@gmail.com>
2020-06-08 20:00:49 -0300

    :children_crossing: Improvind user experience
    Politics Page scrolls up at be redirected

--- src/Components/GradientHeader/index.js
+++ src/Components/GradientHeader/index.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {Link} from 'react-router-dom';
 import "./styles.scss";
 
 export default function GradientHeader(logged) {
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    });
     return (
         <section id="gradientheader">
             <nav>
