8a157cbd4f37ce0e4cdc74366e29034c4b2806bf
KZTN <kakaiocesar2@gmail.com>
2020-06-09 13:12:08 -0300

    :sparkles: Added new Page: TOS
    All changes are functional

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,8 @@ import Form from "./pages/Form";
 import Welcome from "./pages/Welcome";
 import Enviado from "./pages/Enviado";
 import Politics from "./pages/Politics";
+import TOS from "./pages/TOS";
+import Profile from "./pages/Profile";
 
 export default function Routes() {
   return (
@@ -14,6 +16,9 @@ export default function Routes() {
         <Route path="/questionario" component={Form} />
         <Route path="/enviado" component={Enviado} />
         <Route path="/politicas-de-privacidade" component={Politics} />
+        <Route path="/termos-de-uso" component={TOS} />
+        <Route path="/eu" component={Profile} />
+
       </Switch>
     </BrowserRouter>
   );
